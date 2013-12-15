2002ccfa0470d2bfd06f579c76ce4099d47944b5
bliti <pablo.rivera.programmer@gmail.com>
2013-12-14 23:25:33 -0400

    detail views

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, url
-from todo.views import CollectionList, ItemList
+from todo.views import CollectionList, ItemList, CollectionDetail, ItemDetail
 
 
 urlpatterns = patterns('todo.views',
-    url(r'^collections/$', CollectionList.as_view()),
-    url(r'^items/$', ItemList.as_view()),
+    url(r'^collections/$', CollectionList.as_view(), name="collections"),
+    url(r'^items/$', ItemList.as_view(), name="items"),
+    url(r'^collection/(?P<pk>\w+)/$',
+        CollectionDetail.as_view(), 
+        name="collection_detail"),
+    url(r'^item/(?P<pk>\w+)/$',
+        ItemDetail.as_view(), 
+        name="item_detail"),    
     )
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from models import Collection, Item
 
 
@@ -9,7 +9,24 @@ class CollectionList(ListView):
     template_name = 'collection_list.html'
 
 
+class CollectionDetail(DetailView):
+    """view a specific collection"""
+    model = Collection
+    template_name = 'collection_view.html'
+
+
+    def get_queryset(self):
+        """return all related items to a collection"""
+        return get_object_or_404(Collection,id=self.kwargs['pk']).item_set.all()
+
+
 class ItemList(ListView):
     "view all todo items"
     model = Item
     template_name = 'item_list.html'
+
+
+class ItemDetail(DetailView):
+    """view a specific item"""
+    model = Item
+    template_name = "item_view.html"
